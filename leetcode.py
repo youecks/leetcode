b48ed190952b2f904b5209f9240d8e76ab4c9896
Ecks <ecks@Eckss-MBP.attlocal.net>
2023-10-02 10:19:07 -0400

    Linked List Find Solution

--- leetcode.py
+++ leetcode.py
@@ -99,3 +99,50 @@ def sum_list(head):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+######################
+## Linked List Find ##
+######################
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# Iterative
+def linked_list_find(head, target):
+  
+    current = head
+  
+    while current is not None:
+        if current.val == target:
+            return True 
+        current = current.next 
+    return False
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(1)
+
+# Recursive
+
+def linked_list_find(head, target):
+  if head is None:
+    return False
+  if head.val == target:
+    return True
+  return linked_list_find(head.next, target)
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
--- test.py
+++ test.py
@@ -1,21 +1,23 @@
 class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+def linked_list_find(head, target):
   
-a = Node('A')
-b = Node('B')
+    current = head
   
-print(a)
-    
-
-
-
-
-
-
-
-
-
-
-
+    while current is not None:
+        if current.val == target:
+            return True 
+        current = current.next 
+    return False
