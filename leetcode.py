e94218d9bcf591c7f7164025f3707b1f89206a7d
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-04 09:22:51 -0400

    Longest Streak Solution

--- leetcode.py
+++ leetcode.py
@@ -369,3 +369,38 @@ def is_univalue_list(head, prev_val = None):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+####################
+## Longest Streak ##
+####################
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def longest_streak(head):
+  max_streak = 0
+  current_streak = 0
+  prev_val = None
+  
+  current_node = head
+  while current_node is not None:
+    if current_node.val == prev_val:
+      current_streak += 1
+    else:
+      current_streak = 1
+  
+    prev_val = current_node.val
+    if current_streak > max_streak:
+      max_streak = current_streak
+    
+    current_node = current_node.next
+    
+  return max_streak
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(1)
+
