606b9c2ecbe0b504a3274a4fedab4d0fdb77c395
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-05 08:47:22 -0400

    Insert Node Solution

--- leetcode.py
+++ leetcode.py
@@ -452,3 +452,67 @@ def remove_node(head, target_val):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+#################
+## Insert Node ##
+#################
+
+# Iterative
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def insert_node(head, value, index):
+    if index == 0:
+        new_head = Node(value)
+        new_head.next = head
+        return new_head
+
+    count = 0
+    current = head
+    while current is not None:
+        if count == index - 1:
+            temp = current.next
+            current.next = Node(value)
+            current.next.next = temp
+
+        count += 1
+        current = current.next
+
+    return head
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(1)
+
+# Recursive
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def insert_node(head, value, index, count = 0):
+  if index == 0:
+    new_head = Node(value)
+    new_head.next = head
+    return new_head
+  
+  if head is None:
+    return None
+  
+  if count == index - 1:
+      temp = head.next
+      head.next = Node(value)
+      head.next.next = temp
+      return 
+  
+  insert_node(head.next, value, index, count + 1)
+  return head
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
