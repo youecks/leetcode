939ce32c41f7313fed13f918f1954be682c339ff
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-05 10:39:42 -0400

    Create Linked List Solution

--- leetcode.py
+++ leetcode.py
@@ -516,3 +516,44 @@ def insert_node(head, value, index, count = 0):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+########################
+## Create Linked List ##
+########################
+
+# Iterative 
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def create_linked_list(values):
+    dummy_head = Node(None)
+    tail = dummy_head
+    
+    for val in values:
+        tail.next = Node(val)
+        tail = tail.next
+    
+    return dummy_head.next
+
+# n = length of values
+# Time: O(n)
+# Space: O(n)
+
+# Recursive
+
+def create_linked_list(values, i = 0):
+  if i == len(values):
+    return None
+  head = Node(values[i])
+  head.next = create_linked_list(values, i + 1)
+  return head
+
+# n = length of values
+# Time: O(n)
+# Space: O(n)
+
+
