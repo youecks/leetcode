119fe8822c1d730a5872ffcfaa2d378a002a8aa6
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-06 22:41:46 -0400

    Tree Sum Solution

--- test.py
+++ test.py
@@ -1,20 +1,49 @@
-# class Node:
-#   def __init__(self, val):
-#     self.val = val
-#     self.left = None
-#     self.right = None
+# Recursive
 
-def breadth_first_values(root):
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def tree_sum(root):
     if root is None:
-        return []
-    values = []
-    queue = [ root ]
-    while queue:
-        current = queue.pop(0)
-        values.append(current.val)
+        return 0
+    return root.val + tree_sum(root.left) + tree_sum(root.right)
 
-        if current.left:
-            queue.append(current.left)
-        if current.right:
-            queue.append(current.right)
-    return values
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
+
+# Iterative
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+from collections import deque
+
+def tree_sum(root):
+  if not root:
+    return 0
+
+  queue = deque([ root ])
+  total_sum = 0
+  while queue:
+    node = queue.popleft()
+
+    total_sum += node.val
+
+    if node.left:
+      queue.append(node.left)
+
+    if node.right:
+      queue.append(node.right)
+
+  return total_sum
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
