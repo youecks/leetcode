a747452432f577acd5f0bedc6b273fce82769083
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-07 22:10:04 -0400

    Tree Min Value Solution

--- leetcode.py
+++ leetcode.py
@@ -807,3 +807,29 @@ def tree_includes(root, target):
 # Space: O(n)
 
 
+####################
+## Tree Min Value ##
+####################
+
+# Depth First Iterative
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def tree_min_value(root):
+  stack = [ root ]
+  smallest = float("inf")
+  while stack:
+    current = stack.pop()
+    if current.val < smallest:
+      smallest = current.val
+
+    if current.left is not None:
+      stack.append(current.left)
+    if current.right is not None:
+      stack.append(current.right)
+
+  return smallest
