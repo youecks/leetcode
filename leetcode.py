4fcc6c7d4a2e57be4878a264da663553b0e53782
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-07 21:48:10 -0400

    Tree Includes Solution

--- leetcode.py
+++ leetcode.py
@@ -757,3 +757,53 @@ def tree_sum(root):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+###################
+## Tree Includes ##
+###################
+
+# Breadth First Search
+
+from collections import deque
+
+def tree_includes(root, target):
+  if not root:
+    return False
+  
+  queue = deque([ root ])
+  
+  while queue:
+    node = queue.popleft()
+    
+    if node.val == target:
+      return True
+    
+    if node.left:
+      queue.append(node.left)
+      
+    if node.right:
+      queue.append(node.right)
+      
+  return False
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
+
+# Depth first Search
+
+def tree_includes(root, target):
+  if not root:
+    return False
+  
+  if root.val == target:
+    return True
+  
+  return tree_includes(root.left, target) or tree_includes(root.right, target)
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
+
+
