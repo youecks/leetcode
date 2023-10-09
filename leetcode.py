e2321201bb65b67c5a70a4fa71204222e8f5b3c8
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-08 20:22:23 -0400

    Tree Path Finder Solution

--- leetcode.py
+++ leetcode.py
@@ -864,4 +864,43 @@ def max_path_sum(root):
 # Time: O(n)
 # Space: O(n)
 
+
+######################
+## Tree Path Finder ##
+######################
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def path_finder(root, target):
+  result = _path_finder(root, target)
+  if result is None:
+    return None
+  else:
+    return result[::-1]
+
+def _path_finder(root, target):
+  if root is None:
+    return None
   
+  if root.val == target:
+    return [ root.val ]
+  
+  left_path = _path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path
+  
+  right_path = _path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  
+  return None
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
--- test.py
+++ test.py
@@ -1,22 +1,31 @@
-# Depth First (Recursive)
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
 
-class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+def path_finder(root, target):
+  result = _path_finder(root, target)
+  if result is None:
+    return None
+  else:
+    return result [::-1]
 
-def max_path_sum(root):
+def _path_finder(root, target):
   if root is None:
-    return float("-inf")
+    return None
 
-  if root.left is None and root.right is None:
-    return root.val
-
-  return root.val + max(max_path_sum(root.left), max_path_sum(root.right))
-
-# n = number of nodes
-# Time: O(n)
-# Space: O(n)
-
+  if root.val == target:
+    return [ root.val ]
   
+  left_path = path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path
+  
+  right_path = path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  
+  return None
