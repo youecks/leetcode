897d46796fd75d82d4595558bb3b3514d219e719
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-10 09:05:08 -0400

    Levels Averages Solution

--- leetcode.py
+++ leetcode.py
@@ -1064,3 +1064,40 @@ def tree_levels(root):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+#####################
+## Levels Averages ##
+#####################
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from statistics import mean
+
+def level_averages(root):
+  levels = []
+  fill_levels(root, levels, 0)
+  avgs = []
+  for level in levels:
+    avgs.append(mean(level))
+  return avgs
+
+def fill_levels(root, levels, level_num):
+  if root is None:
+    return
+
+  if len(levels) == level_num:
+    levels.append([ root.val ])
+  else:
+    levels[level_num].append(root.val)
+
+  fill_levels(root.left, levels, level_num + 1)
+  fill_levels(root.right, levels, level_num + 1)
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
