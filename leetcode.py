b6bd98dbb4d8c6467925bc033bd0dc3d44310998
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-09 22:16:40 -0400

    Bottom Right Value Solution

--- leetcode.py
+++ leetcode.py
@@ -939,7 +939,6 @@ def tree_value_count(root, target):
 # Space: O(n)
 
 
-
 ##############
 ## How High ##
 ##############
@@ -961,3 +960,30 @@ def how_high(node):
 # n = number of nodes
 # Time: O(n)
 # Space: O(n)
+
+
+########################
+## Bottom Right Value ##
+########################
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+  
+from collections import deque
+
+def bottom_right_value(root):
+  queue = deque([root])
+
+  while queue:
+    current = queue.popleft()
+
+    if current.left is not None:
+      queue.append(current.left)
+    
+    if current.right is not None:
+      queue.append(current.right)
+
+  return current.val
