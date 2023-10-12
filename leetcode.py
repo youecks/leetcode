d5f3c9a6f9aed0dcb7097d4c9a4ce3c60e3bc8dd
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-11 23:28:12 -0400

    Undirected Path Solution

--- leetcode.py
+++ leetcode.py
@@ -1160,3 +1160,50 @@ def has_path(graph, src, dst):
 # e = number edges
 # Time: O(e)
 # Space: O(n)
+
+
+#####################
+## Undirected Path ##
+#####################
+
+# Depth First
+
+def undirected_path(edges, node_A, node_B):
+  graph = build_graph(edges)
+  return has_path(graph, node_A, node_B, set())
+
+def build_graph(edges):
+  graph = {}
+  
+  for edge in edges:
+    a, b = edge
+    
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+      
+    graph[a].append(b)
+    graph[b].append(a)
+    
+  return graph
+    
+def has_path(graph, src, dst, visited):
+  if src == dst:
+    return True
+  
+  if src in visited:
+    return False
+  
+  visited.add(src)
+  
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst, visited) == True:
+      return True
+    
+  return False
+
+# n = number of nodes
+# e = number edges
+# Time: O(e)
+# Space: O(e)
