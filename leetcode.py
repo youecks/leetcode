c4a08dafa23e934f5d5af16a863477f976c2e34e
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-12 23:31:25 -0400

    Has Path Solution 2

--- leetcode.py
+++ leetcode.py
@@ -1207,3 +1207,31 @@ def has_path(graph, src, dst, visited):
 # e = number edges
 # Time: O(e)
 # Space: O(e)
+
+
+##############
+## Has Path ##
+##############
+
+# Breadth First
+
+from collections import deque
+
+def has_path(graph, src, dst):
+  queue = deque([ src ])
+  
+  while queue:
+    current = queue.popleft()
+    
+    if current == dst:
+      return True
+    
+    for neighbor in graph[current]:
+      queue.append(neighbor)
+    
+  return False
+
+# n = number of nodes
+# e = number edges
+# Time: O(e)
+# Space: O(n)
