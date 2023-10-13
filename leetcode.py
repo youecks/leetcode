2ca407b8a842bc1f05785a2c2f8d9ed9ccd7290a
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-12 23:36:48 -0400

    Connected Components Count

--- leetcode.py
+++ leetcode.py
@@ -1235,3 +1235,36 @@ def has_path(graph, src, dst):
 # e = number edges
 # Time: O(e)
 # Space: O(n)
+
+
+################################
+## Connected Components Count ##
+################################
+
+# Depth First
+
+def connected_components_count(graph):
+  visited = set()
+  count = 0
+  
+  for node in graph:
+    if explore(graph, node, visited) == True:
+      count += 1
+      
+  return count
+
+def explore(graph, current, visited):
+  if current in visited:
+    return False
+  
+  visited.add(current)
+  
+  for neighbor in graph[current]:
+    explore(graph, neighbor, visited)
+  
+  return True
+
+# n = number of nodes
+# e = number edges
+# Time: O(e)
+# Space: O(n)
