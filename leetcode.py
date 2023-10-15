285ee673cee6c563030f27169015d63236c57d13
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-14 21:56:21 -0400

    Uncompress Solution

--- leetcode.py
+++ leetcode.py
@@ -1301,3 +1301,29 @@ def explore_size(graph, node, visited):
 # e = number edges
 # Time: O(e)
 # Space: O(n)
+
+
+################
+## Uncompress ##
+################
+
+def uncompress(s):
+  numbers = '0123456789'
+  result = []
+  i = 0
+  j = 0
+  while j < len(s):
+    if s[j] in numbers:
+      j += 1
+    else:      
+      num = int(s[i:j])
+      result.append(s[j] * num)
+      j += 1
+      i = j
+      
+  return ''.join(result)
+
+# n = number of groups
+# m = max num found in any group
+# Time: O(n*m)
+# Space: O(n*m)
