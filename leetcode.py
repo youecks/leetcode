fd83af6e086a4cde1c1f01e08f2c9fb81cd054fd
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-18 09:28:43 -0400

    Pair Sum Solution

--- leetcode.py
+++ leetcode.py
@@ -1400,3 +1400,23 @@ def most_frequent_char(s):
 # n = length of string
 # Time: O(n)
 # Space: O(n)
+
+
+##############
+## Pair Sum ##
+##############
+
+def pair_sum(numbers, target_sum):
+  previous_numbers = {}
+  
+  for index, num in enumerate(numbers):
+    complement = target_sum - num
+    
+    if complement in previous_numbers:
+      return (index, previous_numbers[complement])
+    
+    previous_numbers[num] = index
+
+# n = length of numbers list
+# Time: O(n)
+# Space: O(n)
