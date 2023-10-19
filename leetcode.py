9fc419ca84105239017ea0ea98019fc8ef9b6f0d
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-19 08:23:58 -0400

    Shortest Path Solution

--- leetcode.py
+++ leetcode.py
@@ -1420,3 +1420,48 @@ def pair_sum(numbers, target_sum):
 # n = length of numbers list
 # Time: O(n)
 # Space: O(n)
+
+
+###################
+## Shortest Path ##
+###################
+
+# Breadth First
+
+from collections import deque
+
+def shortest_path(edges, node_A, node_B):
+  graph = build_graph(edges)
+  visited = set([ node_A])
+  queue = deque([ ( node_A, 0 ) ])
+
+  while queue:
+    node, distance = queue.popleft()
+
+    if node == node_B:
+      return distance
+    
+    for neighbor in graph[node]:
+      if neighbor not in visited:
+        visited.add(neighbor)
+        queue.append((neighbor, distance + 1))
+
+  return -1
+
+def build_graph(edges):
+  graph = {}
+
+  for edge in edges:
+    a, b = edge
+    if a not in graph:
+      graph[a] = []
+    if b not in graph:
+      graph[b] = []
+    graph[a].append(b)
+    graph[b].append(a)
+
+  return graph
+
+# e = number edges
+# Time: O(e)
+# Space: O(e)
