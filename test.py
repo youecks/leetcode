9903ba3dd38a2b884716589512deff3c5f9181ae
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-19 08:45:17 -0400

    Island Count Solution

---
+++ test.py
@@ -0,0 +1,39 @@
+def island_count(grid):
+    visited = set()
+    count = 0
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if explore(grid, r, c, visited) == True:
+                count += 1
+    return count
+
+def explore(grid, r, c, visited):
+    row_inbounds = 0 <= r < len(grid)
+    col_inbounds = 0 <= c < len(grid[0])
+    if not row_inbounds or not col_inbounds:
+        return False
+    
+    if grid[r][c] == 'W':
+        return False
+    
+    pos = (r, c)
+    if pos in visited:
+        return False
+    visited.add(pos)
+
+    explore(grid, r - 1, c, visited)
+    explore(grid, r + 1, c, visited)
+    explore(grid, r, c - 1, visited)
+    explore(grid, r, c + 1, visited)
+
+    return True
+
+# r = number of rows
+# c = number of columns
+# Time: O(rc)
+# Space: O(rc)
+
+
+    
+
