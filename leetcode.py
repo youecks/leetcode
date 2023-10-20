ebc250d8896ea272587ff3b7c65869884a49824a
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-20 09:31:39 -0400

    Closest Carrot Solution

--- leetcode.py
+++ leetcode.py
@@ -1547,3 +1547,39 @@ def explore_size(grid, r, c, visited):
 # c = number of columns
 # Time: O(rc)
 # Space: O(rc)
+
+
+####################
+## Closest Carrot ##
+####################
+
+from collections import deque
+
+def closest_carrot(grid, starting_row, starting_col):
+  visited = set([ (starting_row, starting_col) ])
+  queue = deque([ (starting_row, starting_col, 0) ])
+  while queue:
+    row, col, distance = queue.popleft()
+    
+    if grid[row][col] == 'C':
+      return distance
+    
+    deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+    for delta in deltas:
+      delta_row, delta_col = delta
+      neighbor_row = row + delta_row
+      neighbor_col = col + delta_col      
+      pos = (neighbor_row, neighbor_col)
+      row_inbounds = 0 <= neighbor_row < len(grid)
+      col_inbounds = 0 <= neighbor_col < len(grid[0])
+      if row_inbounds and col_inbounds and pos not in visited and grid[neighbor_row][neighbor_col] != 'X':
+        visited.add(pos)
+        queue.append((neighbor_row, neighbor_col, distance + 1))
+        
+  return -1
+
+# r = number of rows
+# c = number of columns
+# Time: O(rc)
+# Space: O(rc)
