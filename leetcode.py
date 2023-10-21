6d7d5cce3f0436513233b883667fccdfb57bbc1f
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-21 09:22:42 -0400

    Semesters Required Solution

--- leetcode.py
+++ leetcode.py
@@ -1617,3 +1617,50 @@ def traverse_distance(graph, node, distance):
 # n = # nodes
 # Time: O(e)
 # Space: O(n)
+
+
+########################
+## Semesters Required ##
+########################
+
+def semesters_required(num_courses, prereqs):
+  graph = build_graph(num_courses, prereqs)
+  distance = {}
+  for course in range(num_courses):
+    if len(graph[course]) == 0:
+      distance[course] = 1
+  
+  for course in range(num_courses):
+    traverse_distance(graph, course, distance)
+    
+  return max(distance.values())
+
+def traverse_distance(graph, node, distance):
+  if node in distance:
+    return distance[node]
+  
+  max_distance = 0
+  for neighbor in graph[node]:
+    neighbor_distance = traverse_distance(graph, neighbor, distance)
+    if neighbor_distance > max_distance:
+      max_distance = neighbor_distance
+    
+  distance[node] = 1 + max_distance
+  return distance[node]
+
+def build_graph(num_courses, prereqs):
+  graph = {}
+  
+  for course in range(num_courses):
+    graph[course] = []
+    
+  for prereq in prereqs:
+    a, b = prereq
+    graph[a].append(b)
+  
+  return graph
+
+# p = # prereqs
+# c = # courses
+# Time: O(p)
+# Space: O(c)
