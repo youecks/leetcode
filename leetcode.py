d8fd40500ed6e5171122888d7a5b0f866dca6690
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-21 11:16:40 -0400

    Best Bridge Solution

--- leetcode.py
+++ leetcode.py
@@ -1664,3 +1664,65 @@ def build_graph(num_courses, prereqs):
 # c = # courses
 # Time: O(p)
 # Space: O(c)
+
+
+#################
+## Best Bridge ##
+#################
+
+from collections import deque
+
+def best_bridge(grid):
+  main_island = None
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      potential_island = traverse_island(grid, r, c, set())
+      if len(potential_island) > 0:
+        main_island = potential_island
+  
+  visited = set(main_island)
+  queue = deque([ ])
+  for pos in main_island:
+    r, c = pos
+    queue.append((r, c, 0))
+  
+  while queue:
+    row, col, distance = queue.popleft()
+    if grid[row][col] == 'L' and (row, col) not in main_island:    
+      return distance - 1
+    
+    deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    for delta in deltas:
+      delta_row, delta_col = delta
+      neighbor_row = row + delta_row
+      neighbor_col = col + delta_col
+      neighbor_pos = (neighbor_row, neighbor_col)
+      if inbounds(grid, neighbor_row, neighbor_col) and neighbor_pos not in visited:
+        visited.add(neighbor_pos)
+        queue.append((neighbor_row, neighbor_col, distance + 1))
+  
+def inbounds(grid, row, col):
+  row_inbounds = 0 <= row < len(grid)
+  col_inbounds = 0 <= col < len(grid[0])
+  return row_inbounds and col_inbounds
+  
+def traverse_island(grid, row, col, visited):
+  if not inbounds(grid, row, col) or grid[row][col] == 'W':
+    return visited
+  
+  pos = (row, col)
+  if pos in visited:
+    return visited
+  
+  visited.add(pos)
+  
+  traverse_island(grid, row - 1, col, visited)
+  traverse_island(grid, row + 1, col, visited)
+  traverse_island(grid, row, col - 1, visited)
+  traverse_island(grid, row, col + 1, visited)
+  return visited
+
+# r = number of rows
+# c = number of columns
+# Time: O(rc)
+# Space: O(rc)
