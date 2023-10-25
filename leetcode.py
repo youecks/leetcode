12e077824ebd6788e25edf6563a2c72e5fcf85c7
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-25 08:02:26 -0400

    Five Sort Solution

--- leetcode.py
+++ leetcode.py
@@ -1891,3 +1891,25 @@ def intersection(a, b):
 # n = length of array a, m = length of array b
 # Time: O(n+m)
 # Space: O(n)
+
+
+###############
+## Five Sort ##
+###############
+
+def five_sort(nums):
+ i = 0
+ j = len(nums) - 1
+ while i < j:
+  if nums[j] == 5:
+   j -= 1
+  elif nums[i] == 5:
+   nums[i], nums[j] = nums[j], nums[i]
+   i += 1
+  else:
+   i += 1
+ return nums
+
+# n = array size
+# Time: O(n)
+# Space: O(1)
