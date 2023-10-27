42770836e3d345097e9c3366b315808162a0452b
Ecks <139870030+youecks@users.noreply.github.com>
2023-10-26 21:17:59 -0400

    Is Prime Solution

--- leetcode.py
+++ leetcode.py
@@ -1913,3 +1913,24 @@ def five_sort(nums):
 # n = array size
 # Time: O(n)
 # Space: O(1)
+
+
+##############
+## Is Prime ##
+##############
+
+from math import sqrt, floor
+
+def is_prime(n):
+  if n < 2:
+    return False
+  
+  for i in range(2, floor(sqrt(n)) + 1):
+    if n % i == 0:
+      return False
+    
+  return True
+
+# n = input number
+# Time: O(square_root(n))
+# Space: O(1)
