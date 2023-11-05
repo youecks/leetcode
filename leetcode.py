3343caafd8f4c34f6a104ce836eb0347c2b3a160
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-04 22:30:36 -0400

    Find Pivot Index Solution

--- leetcode.py
+++ leetcode.py
@@ -2270,3 +2270,19 @@ def generate(numRows):
 # n = number of rows
 # Time: O(n^2)
 # Space: O(n^2)
+
+
+######################
+## Find Pivot Index ##
+######################
+
+def pivotIndex(nums):
+    total = sum(nums)
+
+    leftSum = 0
+    for i in range(len(nums)):
+        rightSum = total - nums[i] - leftSum
+        if leftSum == rightSum:
+            return i
+        leftSum += nums[i]
+    return -1
