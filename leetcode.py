acaa3236eb9c13ef0359fe93b8da8dc4c45f2f7a
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-07 22:45:45 -0500

    Unique Email Addresses Solution

--- leetcode.py
+++ leetcode.py
@@ -2385,3 +2385,21 @@ class ParkingSystem:
 # n + m = length of string
 # Time: O(n + m)
 # Space: O(n + m)
+
+
+############################
+## Unique Email Addresses ##
+############################
+
+class Solution:
+    def numUniqueEmails(self, emails: list[str]) -> int:
+        unique_emails: set[str] = set()
+        for email in emails:
+            local_name, domain_name = email.split('@')
+            local_name = local_name.split('+')[0]
+            local_name = local_name.replace('.', '')
+            email = local_name + '@' + domain_name
+            unique_emails.add(email)
+        return len(unique_emails)
+    
+  
