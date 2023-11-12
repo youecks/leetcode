7225e5149871760b6187279afeafcc239313541a
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-11 22:06:51 -0500

    Range Sum Query - Immutable Solution

--- leetcode.py
+++ leetcode.py
@@ -2512,3 +2512,23 @@ class Solution:
             if n > 0:
                 res.append(i + 1)
         return res
+
+
+###################################
+##  Range Sum Query - Immutable  ##
+###################################
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = []
+        cur = 0
+        for n in nums:
+            cur += n
+            self.prefix.append(cur)
+        
+        
+    def sumRange(self, left: int, right: int) -> int:
+        r = self.prefix[right] 
+        l = self.prefix[left - 1] if left > 0 else 0
+        return r - l
