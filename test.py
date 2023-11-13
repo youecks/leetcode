2fc0f36b8de3473dee219e7fccc0ca7c80839a73
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-12 21:27:27 -0500

    Review Day

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        for n in nums:
-            i = abs(n) - 1
-            nums[i] = -1 * abs(nums[i])
+def twoSum(nums, target):
+    
 
-        res = []
-        for i, n in enumerate(nums):
-            if n > 0:
-                res.append(i + 1)
-        return res
+
+twoSum([2,7,11,15], 9)
