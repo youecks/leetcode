296f61bfe89d83f4d06a84be99c4c4ed3cf6acda
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-17 21:55:53 -0500

    review

--- leetcode.py
+++ leetcode.py
@@ -2548,3 +2548,13 @@ def longestCommonPrefix(arr):
         pref += el[idx]
 
 print(longestCommonPrefix(["dog","dacecar","dar"]))
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0 
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
