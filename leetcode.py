b834f5049e7d285e373849172e12abcbfe25f4a9
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-21 21:01:19 -0500

    review

--- leetcode.py
+++ leetcode.py
@@ -2524,3 +2524,16 @@ class Solution:
 ## Review ##
 ############
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for s in strs: 
+            count = [0] * 26 # a ... z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+        
+        return res.values()
---
+++ test.py
@@ -0,0 +1,6 @@
+def removeElement(nums, val):
+    k = 0
+
+    
+
+removeElement([3,2,2,3], 3)
