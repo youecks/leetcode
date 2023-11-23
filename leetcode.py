1d6bdec2811c3c49cef79d9be65bace64e7e3a0d
Ecks <139870030+youecks@users.noreply.github.com>
2023-11-22 22:40:57 -0500

    review

--- leetcode.py
+++ leetcode.py
@@ -2524,16 +2524,29 @@ class Solution:
 ## Review ##
 ############
 
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+from collections import defaultdict
 
-        for s in strs: 
-            count = [0] * 26 # a ... z
+def groupAnagrams(strs):
+    my_dict = defaultdict(list)
+    # for each string in the array
+    for string in strs:
 
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-
-            res[tuple(count)].append(s)
+        # array of 26 0's
+        count = [0]*26
         
-        return res.values()
+        # for each character in the string
+        for char in string:
+            
+            # asky value for the char is an integer 
+            # count[0] += 1
+            # add 1 to the index listed
+            count[ord(char) - ord('a')] += 1
+            
+            # key is the array    value is the string
+        my_dict[tuple(count)].append(string)
+
+    # return all values in the dictionary
+    return my_dict.values()
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat", "tab", "by"]))
+    
