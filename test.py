c29c17847ff8f2f6117885dc1446a75e3b13c8ea
Ecks <139870030+youecks@users.noreply.github.com>
2024-01-03 22:11:43 -0500

    review

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 def longestCommonPrefix(arr):
-    
+    pref = ''
+    for i in range(len(arr[1])):
+        for el in arr:
+           if el[i] != arr[1][i]:
+               return pref
+        pref += el[i]
 
 
-longestCommonPrefix(["flower","flow","flight"])
+print(longestCommonPrefix(["flower","flow","floght"]))
