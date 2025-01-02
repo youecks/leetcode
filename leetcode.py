a7ea6c52c1a81e92a52786d52bbb1ed8a38dc00f
Ecks <139870030+youecks@users.noreply.github.com>
2025-01-01 22:27:19 -0500

    review

--- leetcode.py
+++ leetcode.py
@@ -2524,29 +2524,19 @@ class Solution:
 ## Review ##
 ############
 
-from collections import defaultdict
+####################
+## Remove Element ##
+####################
 
-def groupAnagrams(strs):
-    my_dict = defaultdict(list)
-    # for each string in the array
-    for string in strs:
+def removeElement(nums, val):
+    k = 0 
 
-        # array of 26 0's
-        count = [0]*26
-        
-        # for each character in the string
-        for char in string:
-            
-            # asky value for the char is an integer 
-            # count[0] += 1
-            # add 1 to the index listed
-            count[ord(char) - ord('a')] += 1
-            
-            # key is the array    value is the string
-        my_dict[tuple(count)].append(string)
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
 
-    # return all values in the dictionary
-    return my_dict.values()
 
-print(groupAnagrams(["eat","tea","tan","ate","nat","bat", "tab", "by"]))
-    
+# Time: O(n)
+# Space: O(1)
