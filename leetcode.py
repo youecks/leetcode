8a237034ede937fab7489a9856d1d9f3db6d8a29
Ecks <139870030+youecks@users.noreply.github.com>
2025-01-02 21:44:16 -0500

    review

--- leetcode.py
+++ leetcode.py
@@ -2540,3 +2540,26 @@ def removeElement(nums, val):
 
 # Time: O(n)
 # Space: O(1)
+
+
+#############
+## Two Sum ##
+#############
+
+def twoSum(nums, target):
+  prev_map = {}
+
+  for i, num in enumerate(nums, 0):
+    pair = target - num
+    print(pair)
+
+    if pair in prev_map:
+      return (prev_map[pair], i)
+    
+    prev_map[num] = i
+
+# n = number of elements 
+# Time: O(n)
+# Space: O(n)
+
+   
